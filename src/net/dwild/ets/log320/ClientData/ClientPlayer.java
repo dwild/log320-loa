841a9863e46457a902fb9b5b2523e2ad4ce1bbf6
Guillaume Voisine <guillaume.voisine@gmail.com>
2015-03-02 14:07:51 -0500

    Renommé des variables dans ClienPlayer

--- src/net/dwild/ets/log320/ClientData/ClientPlayer.java
+++ src/net/dwild/ets/log320/ClientData/ClientPlayer.java
@@ -4,34 +4,34 @@ import net.dwild.ets.log320.Server.IProtocol;
 
 
 public class ClientPlayer {
-	private IProtocol protocol;
-	
-	public ClientPlayer(IProtocol protocol) {
-		this.protocol = protocol;
-	}
-	
-	public void initConnexion() {
-		protocol.connect();
-	}
-	
-	public boolean isConnected() {
-		return protocol.isConnected();
-	}
-	
-	public void end() {
-		protocol.disconnect();
-	}
-	
-	public char getCommand() {
-		char cmd = protocol.readCMD();
-		return cmd;
-	}
-	
-	public int[][] getBoard() {
-		String answer = protocol.readLine(1024);
-		
-		int[][] board = new int[8][8];
-		String[] boardValues;	
+ private IProtocol protocol;
+ 
+ public ClientPlayer(IProtocol protocol) {
+  this.protocol = protocol;
+ }
+ 
+ public void initConnexion() {
+  protocol.connect();
+ }
+ 
+ public boolean isConnected() {
+  return protocol.isConnected();
+ }
+ 
+ public void end() {
+  protocol.disconnect();
+ }
+ 
+ public char getCommand() {
+  char cmd = protocol.readCMD();
+  return cmd;
+ }
+ 
+ public int[][] getBoard() {
+  String answer = protocol.readLine(1024);
+  
+  int[][] board = new int[8][8];
+  String[] boardValues; 
         boardValues = answer.split(" ");
         
         int x = 0, y = 0;
@@ -45,22 +45,22 @@ public class ClientPlayer {
             }
         }
         
-		return board;
-	}
-	
-	public TurnPlay getTurnOpponent() {
-		String answer = protocol.readLine(16);
-		answer = answer.replaceAll("\\s","");
-		
-		int indexOfHyphen = answer.indexOf("-");
-		String firstMove = answer.substring(0, indexOfHyphen);
-		String secondMove = answer.substring(indexOfHyphen + 1);
-		
-		return new TurnPlay(new Square(firstMove), new Square(secondMove));			
-	}
+  return board;
+ }
+ 
+ public TurnPlay getTurnOpponent() {
+  String answer = protocol.readLine(16);
+  answer = answer.replaceAll("\\s","");
+  
+  int indexOfHyphen = answer.indexOf("-");
+  String from = answer.substring(0, indexOfHyphen);
+  String to = answer.substring(indexOfHyphen + 1);
+  
+  return new TurnPlay(new Square(from), new Square(to));   
+ }
 
-	
-	public void sendTurn(TurnPlay turn) {
-		protocol.send(turn.toString());
-	}
+ 
+ public void sendTurn(TurnPlay turn) {
+  protocol.send(turn.toString());
+ }
 }
